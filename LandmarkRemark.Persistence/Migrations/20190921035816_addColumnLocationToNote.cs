d68c8536f4b7d7f11fb79cc6d179c624b5091b6b
preet.cheema@outlook.com <23CheemaShiv79>
2019-09-21 15:49:01 +1000

    added more logic

---
+++ LandmarkRemark.Persistence/Migrations/20190921035816_addColumnLocationToNote.cs
@@ -0,0 +1,24 @@
+﻿using GeoAPI.Geometries;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LandmarkRemark.Persistence.Migrations
+{
+    public partial class addColumnLocationToNote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<IPoint>(
+                name: "Location",
+                table: "Notes",
+                type: "geometry",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Notes");
+        }
+    }
+}
